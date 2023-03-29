55d2660466b3d7fedb064d2d272d0dbd425bb655
Vitor Galdino <httpvittor@gmail.com>
2023-03-29 09:02:48 -0300

    refactor: change string to double quote

--- exceptions.py
+++ exceptions.py
@@ -1,13 +1,13 @@
 class NegativeTitlesError(Exception):
     def __init__(self):
-        super().__init__('titles cannot be negative')
+        super().__init__("titles cannot be negative")
 
 
 class InvalidYearCupError(Exception):
     def __init__(self):
-        super().__init__('there was no world cup this year')
+        super().__init__("there was no world cup this year")
 
 
 class ImpossibleTitlesError(Exception):
     def __init__(self):
-        super().__init__('impossible to have more titles than disputed cups')
+        super().__init__("impossible to have more titles than disputed cups")
