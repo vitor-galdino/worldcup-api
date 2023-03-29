af0c5757cd18b01ab86a050f99ef9c3b63152425
Vitor Galdino <httpvittor@gmail.com>
2023-03-29 09:09:10 -0300

    feat: move files to teams package

---
+++ teams/utils.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from teams.exceptions import ImpossibleTitlesError, InvalidYearCupError, NegativeTitlesError
+
+
+def data_processing(data):
+    if data["titles"] < 0:
+        raise NegativeTitlesError()
+
+    first_cup = datetime.strptime(data["first_cup"], "%Y-%m-%d")
+    if first_cup.year < 1930 or (first_cup.year - 1930) % 4 != 0:
+        raise InvalidYearCupError()
+
+    now = datetime.now()
+    max_titles = (now.year - first_cup.year) // 4 + 1
+    if data["titles"] > max_titles:
+        raise ImpossibleTitlesError()
