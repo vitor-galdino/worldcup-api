f0d8f98ba57f52c06011b9844ef3782c951147fa
Vitor Galdino <httpvittor@gmail.com>
2023-03-29 16:48:43 -0300

    fix: change titles to be optional

--- teams/models.py
+++ teams/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=30)
-    titles = models.IntegerField(default=0, blank=True)
+    titles = models.IntegerField(default=0, null=True)
     top_scorer = models.CharField(max_length=50)
     fifa_code = models.CharField(max_length=3, unique=True)
     first_cup = models.DateField(null=True)
