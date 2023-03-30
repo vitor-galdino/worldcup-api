67bb45be8cf63da40df4148764cc1b7da07be5e5
Vitor Galdino <httpvittor@gmail.com>
2023-03-30 16:45:49 -0300

    feat: create TeamView and post route

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.forms.models import model_to_dict
+from rest_framework.views import APIView, Response, status
 
-# Create your views here.
+from .models import Team
+
+
+class TeamView(APIView):
+    def post(self, request):
+        team = Team.objects.create(**request.data)
+        return Response(model_to_dict(team), status.HTTP_201_CREATED)
