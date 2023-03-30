5ffdd12ffd97688dc3e3756f6e4a9fe14ebc5bff
Vitor Galdino <httpvittor@gmail.com>
2023-03-30 19:18:31 -0300

    feat: add get method to retrieve team by id

--- teams/views.py
+++ teams/views.py
@@ -1,10 +1,11 @@
 from django.forms.models import model_to_dict
 from rest_framework.views import APIView, Response, status
 
-from teams.exceptions import ImpossibleTitlesError, InvalidYearCupError, NegativeTitlesError
+from teams.exceptions import (ImpossibleTitlesError, InvalidYearCupError,
+                              NegativeTitlesError)
 
-from .utils import data_processing
 from .models import Team
+from .utils import data_processing
 
 
 class TeamView(APIView):
@@ -23,3 +24,15 @@ class TeamView(APIView):
         teams_list = [model_to_dict(team) for team in teams]
 
         return Response(teams_list, status.HTTP_200_OK)
+    
+class TeamDetailView(APIView):
+    def get(self, request, team_id):
+        try:
+            team = Team.objects.get(pk=team_id)
+
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        
+        return Response(model_to_dict(team))
+    
+    
