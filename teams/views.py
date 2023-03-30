89fcd9cdf7066878b36dd1d977e66a5e3a528d8b
Vitor Galdino <httpvittor@gmail.com>
2023-03-30 17:22:25 -0300

    feat: create get route on TeamView

--- teams/views.py
+++ teams/views.py
@@ -7,4 +7,11 @@ from .models import Team
 class TeamView(APIView):
     def post(self, request):
         team = Team.objects.create(**request.data)
+        
         return Response(model_to_dict(team), status.HTTP_201_CREATED)
+    
+    def get(self, request):
+        teams = Team.objects.all()
+        teams_list = [model_to_dict(team) for team in teams]
+
+        return Response(teams_list, status.HTTP_200_OK)
