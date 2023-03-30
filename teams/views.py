271f5e7e4609c8422ced136dc650ea446db88b55
Vitor Galdino <httpvittor@gmail.com>
2023-03-30 18:20:39 -0300

    feat: include error handling

--- teams/views.py
+++ teams/views.py
@@ -1,12 +1,20 @@
 from django.forms.models import model_to_dict
 from rest_framework.views import APIView, Response, status
 
+from teams.exceptions import ImpossibleTitlesError, InvalidYearCupError, NegativeTitlesError
+
+from .utils import data_processing
 from .models import Team
 
 
 class TeamView(APIView):
     def post(self, request):
-        team = Team.objects.create(**request.data)
+        try:
+            data_processing(request.data)
+            team = Team.objects.create(**request.data)
+
+        except (NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError) as err:
+            return Response({"error": str(err)}, status.HTTP_400_BAD_REQUEST)
         
         return Response(model_to_dict(team), status.HTTP_201_CREATED)
     
